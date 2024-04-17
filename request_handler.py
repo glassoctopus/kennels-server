8627dfe75f9421dea1c98e307abe606050e511e8
Thomas McMahon <glassoctopus@yahoo.com>
2024-04-16 21:04:18 -0500

    chapter 4 done

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer
+from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer, create_animal, create_location, create_employee, create_customer
 import json
 
 # Here's a class. It inherits from another class.
@@ -99,15 +99,36 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server
-        """
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        asset = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            asset = create_animal(post_body)
+        
+        elif resource == "locations":
+            asset = create_location(post_body)
+            
+        elif resource == "employees":
+            asset = create_employee(post_body)
+            
+        elif resource == "customers":
+            asset = create_customer(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(json.dumps(asset).encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
--- views/customer_requests.py
+++ views/customer_requests.py
@@ -35,6 +35,10 @@ CUSTOMERS = [
         "id": 9,
         "name": "Bubp Braydon"
     },
+    {
+        "id": 10,
+        "name": "Ryan Tanay"
+    },
 ]
 
 def get_all_customers():
@@ -54,3 +58,19 @@ def get_single_customer(id):
             requested_customer = customer
 
     return requested_customer
+
+def create_customer(customer):
+    # Get the id value of the last customer in the list
+    max_id = CUSTOMERS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the customer dictionary
+    customer["id"] = new_id
+
+    # Add the customer dictionary to the list
+    CUSTOMERS.append(customer)
+
+    # Return the dictionary with `id` property added
+    return customer
